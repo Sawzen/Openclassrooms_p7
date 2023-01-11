2cb24d119636d000bff518bfd7a3b2417f16e7a9
hajar <hager.elharty@outlook.fr>
2023-01-11 19:37:34 +0100

    erreurs corrigés

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ import pandas as pd
 from sklearn import preprocessing
 
 app = Flask(__name__)
-modelfile = 'final_prediction'
+modelfile = 'RF_final_prediction'
 model = p.load(open(modelfile, 'rb'))
 x_test = p.load(open("X_test", 'rb'))
 std_scale = p.load(open("std_scale", 'rb'))
 
 @app.route('/prediction/<client_id>', methods=['GET'])
 def prediction(client_id):
-    #client_feat = requests.get_json(x_test[x_test["SK_ID_CURR"] == client_id])
     client_id = int(client_id)
     client_feat = x_test.loc[x_test["SK_ID_CURR"] == client_id]
     client_feat = client_feat.drop(columns ="SK_ID_CURR")
