c4856c17db35142007ccd54080c8257001a16609
sawzen <hager.elharty@outlook.fr>
2023-01-14 16:04:31 +0100

    modification code API

--- app.py
+++ app.py
@@ -10,14 +10,12 @@ app = Flask(__name__)
 modelfile = 'final_prediction'
 model = p.load(open(modelfile, 'rb'))
 x_test = p.load(open("X_test", 'rb'))
-std_scale = p.load(open("std_scale", 'rb'))
 
 @app.route('/prediction/<client_id>', methods=['GET'])
 def prediction(client_id):
     client_id = int(client_id)
     client_feat = x_test.loc[x_test["SK_ID_CURR"] == client_id]
     client_feat = client_feat.drop(columns ="SK_ID_CURR")
-    client_feat = std_scale.transform(client_feat)      
     prediction = np.array2string(model.predict(client_feat))
     return jsonify(prediction)
 
